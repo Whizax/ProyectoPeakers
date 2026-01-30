a4b97203be28e98848f3b00260ba4037cafc65c7
Genivieven <carmenporrashiguero@gmail.com>
2026-01-30 11:59:10 +0100

    mejora carrusel

--- main/script.js
+++ main/script.js
@@ -4,3 +4,24 @@ var flkty = new Flickity( elem, {
   cellAlign: 'left',
   contain: true
 });
+document.querySelectorAll('.carousel-cell').forEach(cell => {
+    const indicator = cell.querySelector('.drag-indicator');
+
+    cell.addEventListener('mousemove', e => {
+        const rect = cell.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        indicator.style.left = `${x}px`;
+        indicator.style.top = `${y}px`;
+    });
+
+    cell.addEventListener('mouseenter', () => {
+        indicator.style.opacity = '1';
+    });
+
+    cell.addEventListener('mouseleave', () => {
+        indicator.style.opacity = '0';
+    });
+});
